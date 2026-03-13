5e2f09350b997d640c8713089c0e344d0c1764d3
root <root@srv1129310.hstgr.cloud>
2026-03-13 05:48:47 +0000

    latest changes

--- cannabis_management/api/inventory_dashboard.py
+++ cannabis_management/api/inventory_dashboard.py
@@ -30,3 +30,30 @@ def get_stock_by_item_group(item_group, _=None):
             "low_stock_items": low_stock
         }
     }
+
+@frappe.whitelist(allow_guest=True)
+def get_batch_warehouse_summary(item_group):
+    """Returns count of unique project (batch) dimensions per warehouse with positive stock."""
+    data = frappe.db.sql("""
+        SELECT
+            warehouse,
+            COUNT(DISTINCT project) AS batch_count
+        FROM (
+            SELECT
+                sle.warehouse,
+                sle.project,
+                SUM(sle.actual_qty) AS qty
+            FROM `tabStock Ledger Entry` sle
+            INNER JOIN `tabItem` i ON i.name = sle.item_code
+            WHERE i.item_group = %s
+              AND i.disabled = 0
+              AND sle.is_cancelled = 0
+              AND sle.project IS NOT NULL
+              AND sle.project != ''
+            GROUP BY sle.warehouse, sle.project
+            HAVING SUM(sle.actual_qty) > 0
+        ) sub
+        GROUP BY warehouse
+        ORDER BY batch_count DESC
+    """, (item_group,), as_dict=True)
+    return data
