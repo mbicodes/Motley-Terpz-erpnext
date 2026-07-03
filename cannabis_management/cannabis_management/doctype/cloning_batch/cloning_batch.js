9b47cb17009da19ba84ef0d8e6ea1feb7dd3f208
alibajwa007 <babargoraya878@gmail.com>
2026-07-03 21:49:35 +0500

    feat: change js in Clonning Batch Doctype

--- cannabis_management/cannabis_management/doctype/cloning_batch/cloning_batch.js
+++ cannabis_management/cannabis_management/doctype/cloning_batch/cloning_batch.js
@@ -1,52 +1,65 @@
-// Copyright (c) 2026, alltechvirtual.com and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Cloning Batch", {
+    refresh(frm) {
+        calculate_totals(frm);
+    },
 
-frappe.ui.form.on('Cloning Batch', {
-	refresh(frm) {
-		frm.trigger('calculate_totals');
-	},
+    labour_hours(frm) {
+        calculate_totals(frm);
+    },
 
-	session_date(frm) {
-		if (frm.doc.session_date) {
-			const year = (new Date(frm.doc.session_date)).getFullYear()
-			frm.set_value('season', year.toString())
-		}
-		frm.trigger('calculate_totals')
-	},
-
-	labour_hours(frm) { frm.trigger('calculate_totals'); },
-	labour_rate(frm) { frm.trigger('calculate_totals'); },
-
-	mom_plant_details_add(frm) { frm.trigger('calculate_totals'); },
-	mom_plant_details_remove(frm) { frm.trigger('calculate_totals'); },
-	material_details_add(frm) { frm.trigger('calculate_totals'); },
-	material_details_remove(frm) { frm.trigger('calculate_totals'); },
-	clone_details_add(frm) { frm.trigger('calculate_totals'); },
-	clone_details_remove(frm) { frm.trigger('calculate_totals'); },
-
-	calculate_totals(frm) {
-		const f = v => parseFloat(v) || 0;
-		let total_moms = 0;
-		let total_material_qty = 0;
-		let total_material_cost = 0;
-		let total_clones = 0;
-
-		(frm.doc.mom_plant_details || []).forEach(r => total_moms += f(r.cuttings_taken));
-		(frm.doc.material_details || []).forEach(r => { total_material_qty += f(r.quantity); total_material_cost += f(r.amount); });
-		(frm.doc.clone_details || []).forEach(r => total_clones += f(r.quantity));
-
-		frm.set_value('total_mom_plants', total_moms);
-		frm.set_value('total_material_quantity', total_material_qty);
-		frm.set_value('total_material_cost', total_material_cost);
-		frm.set_value('total_clone_quantity', total_clones);
-		frm.set_value('total_quantity', total_clones);
-
-		const labour = f(frm.doc.labour_hours) * f(frm.doc.labour_rate);
-		frm.set_value('total_labor_cost', labour);
-		frm.set_value('total_session_material_cost', total_material_cost);
-		frm.set_value('total_clones_produced', Math.floor(total_clones));
-		const session_cost = f(labour) + f(total_material_cost);
-		frm.set_value('total_session_cost', session_cost);
-		frm.set_value('cost_per_clone', (frm.doc.total_clones_produced ? session_cost / frm.doc.total_clones_produced : 0));
-	}
+    labour_rate(frm) {
+        calculate_totals(frm);
+    }
 });
+
+
+frappe.ui.form.on("Cloning Batch Clone Detail", {
+
+    qty(frm) {
+        calculate_totals(frm);
+    },
+
+    clone_details_add(frm) {
+        calculate_totals(frm);
+    },
+
+    clone_details_remove(frm) {
+        calculate_totals(frm);
+    }
+
+});
+
+
+function calculate_totals(frm) {
+
+    let total_clones = 0;
+
+    (frm.doc.clone_details || []).forEach(function(row) {
+        total_clones += cint(row.qty);
+    });
+
+    // Total Labor Cost
+    let labour_cost =
+        flt(frm.doc.labour_hours) *
+        flt(frm.doc.labour_rate);
+
+    // Agar ye fields DocType mein hain to update karega
+    if (frm.fields_dict.total_labor_cost) {
+        frm.set_value("total_labor_cost", labour_cost);
+    }
+
+    if (frm.fields_dict.total_clones_produced) {
+        frm.set_value("total_clones_produced", total_clones);
+    }
+
+    if (frm.fields_dict.total_session_cost) {
+        frm.set_value("total_session_cost", labour_cost);
+    }
+
+    if (frm.fields_dict.cost_per_clone) {
+        frm.set_value(
+            "cost_per_clone",
+            total_clones > 0 ? labour_cost / total_clones : 0
+        );
+    }
+}
