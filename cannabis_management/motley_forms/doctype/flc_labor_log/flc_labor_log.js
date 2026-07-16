28a290dc610ab6f09ca2ac90aa3f6cd3bca8cd2b
alibajwa007 <babargoraya878@gmail.com>
2026-07-16 16:58:55 +0200

    Remove legacy pages, motley_forms doctypes, and web forms

--- cannabis_management/motley_forms/doctype/flc_labor_log/flc_labor_log.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, alltechvirtual.com and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("FLC Labor Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- cannabis_management/motley_forms/doctype/invoice_updation/invoice_updation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, alltechvirtual.com and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Invoice Updation", {
-// 	refresh(frm) {
-
-// 	},
-// });
