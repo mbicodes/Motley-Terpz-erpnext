a8b06b34c71c9916363a1a4baddff511b062a15c
OsamaASidd <osama.siddiqui2017@gmail.com>
2026-08-26 00:02:17 +0200

    License: add licence fields (number/name/type/company/validity/adjust window/flags); delete unused Licence No doctype

--- cannabis_management/cannabis_management/doctype/licence_no/licence_no.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, alltechvirtual.com and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class LicenceNo(Document):
-	pass
