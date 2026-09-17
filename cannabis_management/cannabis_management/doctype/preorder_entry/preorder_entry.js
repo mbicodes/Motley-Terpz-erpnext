9a6d33e938a64bca43a9e96c091c79cfbd91b233
Muhammad Fawad <mfawad85786@gmail.com>
2026-09-17 12:22:47 +0200

    feat: add rate/amount to preorder items, Motley print format, Create Item button
    - Added rate (editable) and amount (auto-calculated) fields to Preorder Item child table
    - Auto-fetch selling price from Item Price on item selection
    - Added "Create Item" button under Actions menu on Preorder Entry form
    - Created Motley-themed custom print format for Preorder Entry with rate/amount columns and grand total

--- cannabis_management/cannabis_management/doctype/preorder_entry/preorder_entry.js
+++ cannabis_management/cannabis_management/doctype/preorder_entry/preorder_entry.js
@@ -21,6 +21,15 @@ frappe.ui.form.on("Preorder Entry", {
 		});
 	},
 
+	refresh(frm) {
+		frm.add_custom_button(__("Create Item"), function () {
+			frappe.new_doc("Item", {
+				item_group: "Packaged goods",
+				is_stock_item: 1,
+			});
+		}, __("Actions"));
+	},
+
 	primary_address(frm) {
 		if (!frm.doc.primary_address) return;
 		const region = guess_region(frm.doc.primary_address);
@@ -29,6 +38,42 @@ frappe.ui.form.on("Preorder Entry", {
 		}
 	},
 });
+
+frappe.ui.form.on("Preorder Item", {
+	qty(frm, cdt, cdn) {
+		calc_amount(frm, cdt, cdn);
+	},
+	rate(frm, cdt, cdn) {
+		calc_amount(frm, cdt, cdn);
+	},
+	item_code(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.item_code) {
+			frappe.call({
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Item Price",
+					filters: {
+						item_code: row.item_code,
+						selling: 1,
+					},
+					fieldname: "price_list_rate",
+				},
+				callback(r) {
+					if (r.message && r.message.price_list_rate) {
+						frappe.model.set_value(cdt, cdn, "rate", r.message.price_list_rate);
+					}
+				},
+			});
+		}
+	},
+});
+
+function calc_amount(frm, cdt, cdn) {
+	let row = locals[cdt][cdn];
+	let amount = flt(row.qty) * flt(row.rate);
+	frappe.model.set_value(cdt, cdn, "amount", amount);
+}
 
 function guess_region(address) {
 	const zip_match = address.match(/\b(9\d{4})\b/);
